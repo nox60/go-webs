370d7bcf75e16190a6870504f9921d5a92f1caf3
nox60 <nox60@hotmail.com>
2020-07-30 23:25:54 +0800

    Update login logic

--- testdb/models/simpleCode.go
+++ testdb/models/simpleCode.go
@@ -1,7 +1,11 @@
 package models
 
 type SimpleCode struct {
-	ResultCode int    `json:"resultCode"  binding:"required"`
-	Msg        string `json:"msg" binding:"required"`
-	Token      string `json:"token" binding:"required"`
+	ResultCode   int    `json:"resultCode"  binding:"required"`
+	Msg          string `json:"msg" binding:"required"`
+	Token        string `json:"token" binding:"required"`
+	Roles        string `json:"roles" binding:"required"`
+	Introduction string `json:"introduction" binding:"required"`
+	Avatar       string `json:"avatar" binding:"required"`
+	Name         string `json:"name" binding:"required"`
 }
