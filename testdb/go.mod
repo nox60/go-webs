826734ff913c82df5abebffbe3716902211b6d16
LiuLi <nox60@hotmail.com>
2020-08-03 16:14:09 +0800

    Add httpbody struct

--- testdb/go.mod
+++ testdb/go.mod
@@ -3,7 +3,6 @@ module testdb
 go 1.13
 
 require (
-	github.com/codegangsta/negroni v1.0.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
--- testdb/others/jwtTester.go
+++ testdb/others/jwtTester.go
@@ -11,15 +11,17 @@ func main() {
 	fmt.Println("test")
 	signedStr := JwtSign("ddddd")
 
-	token, _ := jwt.Parse(signedStr, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
+	//token, _ := jwt.Parse(signedStr, func(token *jwt.Token) (interface{}, error) {
+	//	// Don't forget to validate the alg is what you expect:
+	//	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+	//		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	//	}
+	//
+	//	// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+	//	return []byte("testkey"), nil
+	//})
 
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte("testkey"), nil
-	})
+	token, _ := JwtParse(signedStr)
 
 	fmt.Println("-----")
 	fmt.Println(token)
@@ -29,6 +31,20 @@ func main() {
 	} else {
 		fmt.Println("error")
 	}
+}
+
+func JwtParse(signedStr string) (jwtToken *jwt.Token, err error) {
+	token, err := jwt.Parse(signedStr, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("testkey"), nil
+	})
+
+	return token, err
 }
 
 func JwtSign(subject string) (signedStr string) {
