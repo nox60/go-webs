9b932f590acf57bad622d496ac75cb7b32a6be90
LiuLi <nox60@hotmail.com>
2020-08-03 17:20:48 +0800

    Add httpbody struct

--- testdb/others/jwtTester.go
+++ testdb/others/jwtTester.go
@@ -7,31 +7,31 @@ import (
 	"time"
 )
 
-func main() {
-	fmt.Println("test")
-	signedStr := JwtSign("ddddd")
-
-	//token, _ := jwt.Parse(signedStr, func(token *jwt.Token) (interface{}, error) {
-	//	// Don't forget to validate the alg is what you expect:
-	//	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-	//		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-	//	}
-	//
-	//	// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-	//	return []byte("testkey"), nil
-	//})
-
-	token, _ := JwtParse(signedStr)
-
-	fmt.Println("-----")
-	fmt.Println(token)
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["sub"])
-	} else {
-		fmt.Println("error")
-	}
-}
+//func main() {
+//	fmt.Println("test")
+//	signedStr := JwtSign("ddddd")
+//
+//	//token, _ := jwt.Parse(signedStr, func(token *jwt.Token) (interface{}, error) {
+//	//	// Don't forget to validate the alg is what you expect:
+//	//	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+//	//		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+//	//	}
+//	//
+//	//	// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+//	//	return []byte("testkey"), nil
+//	//})
+//
+//	token, _ := JwtParse(signedStr)
+//
+//	fmt.Println("-----")
+//	fmt.Println(token)
+//
+//	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+//		fmt.Println(claims["sub"])
+//	} else {
+//		fmt.Println("error")
+//	}
+//}
 
 func JwtParse(signedStr string) (jwtToken *jwt.Token, err error) {
 	token, err := jwt.Parse(signedStr, func(token *jwt.Token) (interface{}, error) {
