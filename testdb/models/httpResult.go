bf5cada774d39870b4f104c596c1c43e74f56967
LiuLi <nox60@hotmail.com>
2020-08-04 17:29:57 +0800

    Add httpbody struct

--- testdb/models/httpResult.go
+++ testdb/models/httpResult.go
@@ -1,8 +1,8 @@
 package models
 
 type HttpResult struct {
-	Code  int    `json:"code"  binding:"required"`
-	Msg   string `json:"msg" binding:"required"`
-	Token string `json:"token" binding:"required"`
-	Data  string `json:"data" binding:"required"`
+	Code  int         `json:"code"  binding:"required"`
+	Msg   string      `json:"msg" binding:"required"`
+	Token string      `json:"token" binding:"required"`
+	Data  interface{} `json:"data" binding:"required"`
 }
--- testdb/models/userInfo.go
+++ testdb/models/userInfo.go
@@ -1,6 +1,7 @@
 package models
 
 type UserInfo struct {
+	Code         int    `json:"code" binding:"required"`
 	Introduction string `json:"introduction" binding:"required"`
 	Avatar       string `json:"avatar" binding:"required"`
 	Name         string `json:"name" binding:"required"`
