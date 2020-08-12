7213cfb91fda7e470b093255592449648b81d69c
nox60 <nox60@hotmail.com>
2020-08-12 23:39:59 +0800

    Update login logic

--- testdb/controller/userController.go
+++ testdb/controller/userController.go
@@ -182,28 +182,3 @@ func JsonLogin(c *gin.Context) {
 		})
 	}
 }
-
-func UserInfo(c *gin.Context) {
-
-	resultMsg := new(models.HttpResult)
-	resultMsg.Code = 20000
-	resultMsg.Msg = "获取用户信息成功"
-	//登录成功
-
-	userInfo := new(models.UserInfo)
-
-	userInfo.Introduction = "I am a super administrator"
-	userInfo.Avatar = "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
-	userInfo.Name = "Super Admin"
-	userInfo.Code = 100
-	userInfo.Roles = "[admin]"
-	resultMsg.Data = userInfo
-
-	c.JSON(200, resultMsg)
-}
-
-func Login(c *gin.Context) {
-	resultMsg := new(models.HttpResult)
-	resultMsg.Token = "test"
-	c.JSON(200, resultMsg)
-}
