2a3fb929a5a865ac1a88ee03480535b9d3e64efc
LiuLi <nox60@hotmail.com>
2020-08-18 17:54:32 +0800

    功能点管理

--- testdb/controller/permissionController.go
+++ testdb/controller/permissionController.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"testdb/models"
 )
 
@@ -13,17 +11,21 @@ func ListFunctionsData(c *gin.Context) {
 	resultMsg.Code = 20000
 	resultMsg.Msg = "获取数据成功"
 
-	var fetchDataRequestBody models.FetchDataRequestBody
 	var dataLists models.PageListDataResult
-	functions := make([]models.PermissionNode, 0)
+	functions := make([]models.FunctionNode, 0)
 
-	if err := c.ShouldBindJSON(&fetchDataRequestBody); err != nil {
-		fmt.Println(err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+	//build mock data for test
+	function1 := models.FunctionNode{1, "test1", "/a/11", ""}
+	function2 := models.FunctionNode{2, "test2", "/a/22", ""}
+	function3 := models.FunctionNode{3, "test3", "/a/33", ""}
+	function4 := models.FunctionNode{4, "test4", "/a/44", ""}
+	function5 := models.FunctionNode{5, "test5", "/a/55", ""}
 
-	fetchDataRequestBody.ItemId = -1
+	functions = append(functions, function1)
+	functions = append(functions, function2)
+	functions = append(functions, function3)
+	functions = append(functions, function4)
+	functions = append(functions, function5)
 
 	//results, totalCount, err := dao.RetrieveSampleData(&fetchDataRequestBody)
 
