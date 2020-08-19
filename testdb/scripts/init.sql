d59ddbdf3c283fb78a5c0bf8842c50e6cd1cff50
liuli <nox60@hotmail.com>
2020-08-19 20:30:29 +0800

    Update interface

--- testdb/scripts/init.sql
+++ testdb/scripts/init.sql
@@ -6,6 +6,6 @@ CREATE TABLE tb_functions (
 	`order` int NOT NULL,
 	`name` VARCHAR(200) NULL,
 	`path` VARCHAR(200) NULL,
-	`parent_function_id` INT NOT NULL DEFAULT 0
-  PRIMARY KEY(`function_id`)
+	`parent_function_id` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`function_id`)
 )DEFAULT CHARSET=utf8;
