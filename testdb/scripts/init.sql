6a9527ad5e5a670bf77d0f749c3a3bea65b660c4
liuli <nox60@hotmail.com>
2020-08-19 20:28:07 +0800

    Update interface

---
+++ testdb/scripts/init.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS tb_functions;
+CREATE TABLE tb_functions (
+	`function_id` INT NOT NULL AUTO_INCREMENT,
+	`number` INT NOT NULL,
+	`order` int NOT NULL,
+	`name` VARCHAR(200) NULL,
+	`path` VARCHAR(200) NULL,
+	`parent_function_id` INT NOT NULL DEFAULT 0
+  PRIMARY KEY(`function_id`)
+)DEFAULT CHARSET=utf8;
