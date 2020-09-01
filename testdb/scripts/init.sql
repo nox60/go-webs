ce7a27a9a9c23ec0e2b13fabc0cef84c06b0f192
LiuLi <nox60@hotmail.com>
2020-09-01 18:10:31 +0800

    角色管理

--- testdb/scripts/init.sql
+++ testdb/scripts/init.sql
@@ -9,3 +9,21 @@ CREATE TABLE tb_functions (
 	`parent_function_id` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`function_id`)
 )DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS tb_roles;
+CREATE TABLE tb_roles (
+	`role_id` INT NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(10) NOT NULL,
+	`name` VARCHAR(200) NULL,
+	`status` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`role_id`)
+)DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS tb_roles_functions;
+CREATE TABLE tb_roles_functions (
+    `row_id` INT NOT NULL AUTO_INCREMENT,
+    `role_id` INT NOT NULL,
+    `function_id` INT NOT NULL,
+    `status` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`row_id`)
+)DEFAULT CHARSET=utf8;
