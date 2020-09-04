e0dac773e6254ca845756d4494ed60240eb5da9e
LiuLi <nox60@hotmail.com>
2020-09-04 16:47:14 +0800

    角色管理

--- fortest/main.go
+++ fortest/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type testStruct struct {
+	RoleId    int         `json:"roleId"`
+	Code      string      `json:"code"`
+	Name      string      `json:"name"`
+	Status    int         `json:"status"`
+	Functions interface{} `json:"functionIds"`
+}
+
 func main() {
 	i := 0
 	defer fmt.Println("aaa", i) //输出0，因为i此时就是0
