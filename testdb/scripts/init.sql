8fc3890f149ee84ef6d7218146fa4515ce60f792
liuli <nox60@hotmail.com>
2020-09-20 23:41:42 +0800

    实现权限删除功能

--- testdb/scripts/init.sql
+++ testdb/scripts/init.sql
@@ -36,3 +36,15 @@ CREATE TABLE tb_roles_functions (
     `status` TINYINT NOT NULL DEFAULT 0,
     PRIMARY KEY (`row_id`)
 )DEFAULT CHARSET=utf8;
+
+
+
+
+DROP TABLE IF EXISTS tb_roles_functions;
+CREATE TABLE tb_roles_functions (
+    `row_id` INT NOT NULL AUTO_INCREMENT,
+    `role_id` INT NOT NULL,
+    `function_id` INT NOT NULL,
+    `status` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`row_id`)
+)DEFAULT CHARSET=utf8;
