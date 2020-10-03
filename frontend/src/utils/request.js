08b598399e5305323ca1bcad22d5cc8578347a0b
nox60 <nox60@hotmail.com>
2020-10-03 17:10:38 +0800

    修复部分JS格式

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -7,7 +7,7 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 10000 // request timeout
 })
 
 // request interceptor
@@ -69,7 +69,7 @@ service.interceptors.response.use(
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
       // 刷新Token，更新cookie里面的Token
-      
+
 
 
       return res
