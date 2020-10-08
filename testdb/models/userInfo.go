6b82840707aa08dceddfa23c6ebb5eb7c752d77b
nox60 <nox60@hotmail.com>
2020-10-08 23:20:58 +0800

    用户-权限组表

--- testdb/models/userInfo.go
+++ testdb/models/userInfo.go
@@ -6,10 +6,18 @@ type UserInfo struct {
 	Avatar       string `json:"avatar" binding:"required"`
 	Name         string `json:"name" binding:"required"`
 	Roles        string `json:"roles" binding:"required"`
+	Page         int    `json:"page"  `
+	Limit        int    `json:"limit" `
+	ForCount     bool
 }
 
 type User struct {
 	AccountId int    `json:"accountId"`
 	UserName  string `json:"userName"`
 	RealName  string `json:"realName"`
+}
+
+func (reqBody *UserInfo) GetStartByPageAndLimit() int {
+	result := (reqBody.Page - 1) * reqBody.Limit
+	return result
 }
