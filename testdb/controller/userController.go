fa8e8899f7002d41baf2f9d883996636c088f55d
LiuLi <nox60@hotmail.com>
2020-10-19 16:35:29 +0800

    重构用户登录逻辑

--- testdb/controller/userController.go
+++ testdb/controller/userController.go
@@ -84,6 +84,25 @@ func UserInfo(c *gin.Context) {
 	c.JSON(200, resultMsg)
 }
 
+func LogOut(c *gin.Context) {
+
+	resultMsg := new(models.HttpResult)
+	resultMsg.Code = 20000
+	resultMsg.Msg = "获取用户信息成功"
+	//登录成功
+
+	userInfo := new(models.UserInfo)
+
+	userInfo.Introduction = "I am a super administrator"
+	userInfo.Avatar = "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
+	userInfo.Name = "Super Admin"
+	userInfo.Code = 100
+	userInfo.Roles = "[admin]"
+	resultMsg.Data = userInfo
+
+	c.JSON(200, resultMsg)
+}
+
 func Login(c *gin.Context) {
 	resultMsg := new(models.HttpResult)
 	resultMsg.Token = "test"
