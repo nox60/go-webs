7e4d9c0de4365c5b77192804f5fe3b345dfcdbc5
LiuLi <nox60@hotmail.com>
2020-10-20 17:16:33 +0800

    重构用户登录逻辑

--- testdb/controller/userController.go
+++ testdb/controller/userController.go
@@ -86,21 +86,22 @@ func UserInfo(c *gin.Context) {
 
 func LogOut(c *gin.Context) {
 
-	resultMsg := new(models.HttpResult)
-	resultMsg.Code = 20000
-	resultMsg.Msg = "获取用户信息成功"
-	//登录成功
-
-	userInfo := new(models.UserInfo)
-
-	userInfo.Introduction = "I am a super administrator"
-	userInfo.Avatar = "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
-	userInfo.Name = "Super Admin"
-	userInfo.Code = 100
-	userInfo.Roles = "[admin]"
-	resultMsg.Data = userInfo
-
-	c.JSON(200, resultMsg)
+	// 使用jwt, 后端不用注销。
+	//resultMsg := new(models.HttpResult)
+	//resultMsg.Code = 20000
+	//resultMsg.Msg = "获取用户信息成功"
+	////登录成功
+	//
+	//userInfo := new(models.UserInfo)
+	//
+	//userInfo.Introduction = "I am a super administrator"
+	//userInfo.Avatar = "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
+	//userInfo.Name = "Super Admin"
+	//userInfo.Code = 100
+	//userInfo.Roles = "[admin]"
+	//resultMsg.Data = userInfo
+	//
+	//c.JSON(200, resultMsg)
 }
 
 func Login(c *gin.Context) {
